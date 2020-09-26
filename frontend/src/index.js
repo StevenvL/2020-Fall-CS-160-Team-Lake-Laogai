c89ebc2ec18a3eedca9a0f27719fd14f7d68fbad
su-ching <chingching.kao@sjsu.edu>
2020-09-25 22:21:53 -0700

    Wireframe created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import ConnectionTest from './ConnectionTest'
+import App from './App';
 
 ReactDOM.render(
-	<ConnectionTest />,
+	<App />,
   	document.getElementById('root')
 );
 
