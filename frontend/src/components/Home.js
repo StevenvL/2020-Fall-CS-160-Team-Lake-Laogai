4a0bc928241fa112a5dec2f8b4df6553856708ef
caviarLuv <yashleycaviar@gmail.com>
2020-09-27 21:36:51 -0700

    Adding test connection between react and nodejs

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function Home(){
-    return(
-        <div className="appBody">
-            <h1>Home</h1>
-        </div>
-        
-    )
+class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: ""
+		};
+	}
+
+	testConnection() {
+		fetch('http://localhost:8000/users')
+		.then(response => response.text())
+		.then(json => this.setState({data: json}))
+		.catch(err => err)
+	}
+
+	componentDidMount() {
+		this.testConnection();
+	}
+
+	render() {
+		return (
+			<div>
+			<h1>Home</h1>
+			<p>result fetched: {this.state.data}</p>
+			</div>
+			);
+	}
 }
+
 
 export default Home;
