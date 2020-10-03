68a36175b451b3be6b8e5c7214bb4001aa960482
caviarLuv <yashleycaviar@gmail.com>
2020-10-02 17:52:54 -0700

    testing connection at route 8000/users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../sql_conn');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello fellow boba lovers, sending love to you from the backend server');
+  connection.query("SELECT * FROM users", function(error, results, fields) {
+  	if( error ) throw error;
+  	console.log(results);
+  	res.json(results);
+  });
 });
 
+/*
+const findUser = function(username, connection) {
+	return new Promise(function(resolve, reject){
+		connection.query('SELECT * FROM users')
+		.then(function(result){
+			result.
+		})
+	})
+	.catch(function (error) {
+		reject(error);
+	});
+}
+*/
 module.exports = router;
---
+++ backend/sql_conn.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+
+let connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'laogai',
+  password: 'boba',
+  database: 'pokeboba',
+  port: '3306'
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('Error connecting: ' + err.stack);
+      return;
+    }
+    console.log('Connected as thread id: ' + connection.threadId);
+  });
+ 
+module.exports = connection;
