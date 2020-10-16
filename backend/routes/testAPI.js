4e6de37390cee0f4c2fc53ac063ab8e10362d10a
yuxiu198 <xiuyutiffany@gmail.com>
2020-10-16 11:30:39 -0700

    update testAPI.js

--- backend/routes/testAPI.js
+++ backend/routes/testAPI.js
@@ -7,10 +7,20 @@ router.get('/', function(req, res, next) {
 });
 
 
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//     console.log('We are listing user tables...');
+//     connection.query("SELECT * FROM user", function(error, results, fields) {ç
+//         if( error ) throw error;
+//         console.log(results);
+//         res.json(results);
+//     });
+// });
+
 // load user table
 router.get('/usertable', (req, res) => {
     console.log('We are listing user tables...');
-    let sql = 'SELECT * FROM user;';
+    let sql = 'SELECT * FROM user';
     connection.query(sql, (err, result) => {
         if(err) {
             console.log('Error occurs on listing tables.');
