68b1fd8b5c67663c63718258190eb315906a10ee
su-ching <chingching.kao@sjsu.edu>
2020-10-16 15:45:22 -0700

    Added storeView

--- backend/sql_conn.js
+++ backend/sql_conn.js
@@ -5,7 +5,7 @@ let connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'laogai',
   password: 'boba',
-  database: 'pokeboba',
+  database: 'pokebobaschema',
   port: '3306'
 });
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Owner from './components/Owner'
 import FindStore from './components/FindStore'
 import SignUp from './components/SignUp'
 import Login from './components/Login'
+import StoreView from './components/StoreView'
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 
 
@@ -20,7 +21,12 @@ function App() {
             <Route path="/forums" component={Forums} />
             <Route path="/blogs" component={Blogs} />
             <Route path="/owner" component={Owner} />
+            {/* <Route path="/findStore/:storeName" component={props =>{
+              return <StoreView {...props} />
+            }} /> */}
+            <Route path="/stores/:storeName" component={StoreView} />
             <Route path="/findStore" component={FindStore} />
+           
             <Route path="/login" component={Login}/>
             <Route path="/signUp" component={SignUp} />
           </div>        
