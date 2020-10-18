d6f3061435bcd9739394fb94f54a92a882d3bded
yuxiu198 <xiuyutiffany@gmail.com>
2020-10-17 22:46:41 -0700

    retrieve all stores

--- backend/routes/stores.js
+++ backend/routes/stores.js
@@ -1,6 +1,20 @@
+/*Stores Info Backend APIs*/
 var express = require('express');
 var router = express.Router();
 var connection = require('../sql_conn');
+
+/* GET all stores */
+router.get('/', function(req, res, next) {
+    /*error takes care the error if happened during the query
+	results conatins the results of the query
+	fields contains information about the returned results field
+    */
+    connection.query("SELECT * FROM stores", function(error, results, fields) {
+        if( error ) throw error;
+        console.log(results);
+        res.json(results);
+    });
+});
 
 /* GET stores from store name. */
 router.get('/:storeName', function(req, res, next) {
@@ -13,26 +27,7 @@ router.get('/:storeName', function(req, res, next) {
   });
 });
 
-// /* GET stores from store name. */
-// router.get('/', function(req, res, next) {
-//     connection.query("SELECT storeName FROM stores", function(error, results, fields) {
-//         if( error ) throw error;
-//         console.log(results);
-//         res.json(results);
-//     });
-//   });
 
-/*
-const findUser = function(username, connection) {
-	return new Promise(function(resolve, reject){
-		connection.query('SELECT * FROM users')
-		.then(function(result){
-			result.
-		})
-	})
-	.catch(function (error) {
-		reject(error);
-	});
-}
-*/
+
+
 module.exports = router;
