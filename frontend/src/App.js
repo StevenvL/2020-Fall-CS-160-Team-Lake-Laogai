5955ee0bc6d764e44b95dec7eefa3a84f3ec83f3
Zhijie Xu <jennyxu000@gmail.com>
2020-10-20 00:17:03 -0700

    button group style update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import Header from './components/Header'
-import Home from './components/Home'
-import Forums from './components/Forums'
-import Blogs from './components/Blogs'
-import Owner from './components/Owner'
-import FindStore from './components/FindStore'
-import SignUp from './components/SignUp'
-import Login from './components/Login'
-import StoreView from './components/StoreView'
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
-
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Forums from "./components/Forums";
+import Blogs from "./components/Blogs";
+import Owner from "./components/Owner";
+import FindStore from "./components/FindStore";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import StoreView from "./components/StoreView";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />  
-        <Switch>
-          <div>
-            <Route path="/" exact component={Home} />
-            <Route path="/forums" component={Forums} />
-            <Route path="/blogs" component={Blogs} />
-            <Route path="/owner" component={Owner} />
-            {/* <Route path="/findStore/:storeName" component={props =>{
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/forums" component={Forums} />
+        <Route path="/blogs" component={Blogs} />
+        <Route path="/owner" component={Owner} />
+        {/* <Route path="/findStore/:storeName" component={props =>{
               return <StoreView {...props} />
             }} /> */}
-            <Route path="/stores/:storeName" component={StoreView} />
-            <Route path="/findStore" component={FindStore} />
-           
-            <Route path="/login" component={Login}/>
-            <Route path="/signUp" component={SignUp} />
-          </div>        
-        </Switch>            
+        <Route path="/stores/:storeName" component={StoreView} />
+        <Route path="/findStore" component={FindStore} />
+
+        <Route path="/login" component={Login} />
+        <Route path="/signUp" component={SignUp} />
+      </Switch>
     </BrowserRouter>
-    
   );
 }
 
--- frontend/src/components/FindStore.js
+++ frontend/src/components/FindStore.js
@@ -1,39 +1,43 @@
-import React from 'react';
-import { Container, Jumbotron, Button, ButtonGroup } from 'react-bootstrap'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Container, Jumbotron, Button, ButtonGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-function FindStore(){
-    const dummyStores = ["boba", "sugar"]
-    return(
-        <Container>
-            <Jumbotron className="search-boba">
-               <form className="col-lg-6 offset-lg-3 search-form" action="POST">
-                    <input className="form-control mb-2 mr-sm-2" type="search" placeholder="Search Boba Store" />
-                    <button className="btn btn-normal my-2 my-sm-0" type="submit">Search</button>
-                </form>   
-            </Jumbotron>     
+function FindStore() {
+  const dummyStores = ["store1", "store2"];
 
-           
-            <ButtonGroup size="lg" className="mb-2">
-                <Button className="btn-filter">Organic</Button>
-                <Button>Black Tea</Button>
-                <Button>Green Tea</Button>
-                <Button>Alternative Milk</Button>
-                <Button>Oolong Tea</Button>
-                <Button>Brown Sugar</Button>
+  return (
+    <Container>
+      <Jumbotron className="search-boba">
+        <form className="col-lg-6 offset-lg-3 search-form" action="POST">
+          <input
+            className="form-control mb-2 mr-sm-2"
+            type="search"
+            placeholder="Search Boba Store"
+          />
+          <button className="btn btn-normal my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+      </Jumbotron>
 
-            </ButtonGroup>  
+      <ButtonGroup size="lg" className="mb-2 filterButtons">
+        <Button className="btn-filter button1">Organic</Button>
+        <Button className="btn-filter">Black Tea</Button>
+        <Button className="btn-filter">Green Tea</Button>
+        <Button className="btn-filter">Alternative Milk</Button>
+        <Button className="btn-filter">Oolong Tea</Button>
+        <Button className="btn-filter">Brown Sugar</Button>
+      </ButtonGroup>
 
-           
-            <ul>
-                {dummyStores.map(function(name, index){
-                    return <li key={ index }> 
-                        <Link to={`/stores/${name}`}>{name}</Link></li>;
-                  })}
-            </ul>
-        </Container>
-        
-    )
+      {dummyStores.map(function (name, index) {
+        return (
+          <div className="singleStoreDiv">
+            <Link to={`/stores/${name}`}>{name}</Link>
+          </div>
+        );
+      })}
+    </Container>
+  );
 }
 
 export default FindStore;
