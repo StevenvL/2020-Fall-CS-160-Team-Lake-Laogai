7972dbf0b4fb2d402f690c812a0dd9f10f7ebbd0
yuxiu198 <xiuyutiffany@gmail.com>
2020-10-28 11:50:34 -0700

    pulled from main-branch and use my DB connection

--- backend/sql_conn.js
+++ backend/sql_conn.js
@@ -3,9 +3,9 @@ const mysql = require('mysql');
 
 let connection = mysql.createConnection({
   host: '127.0.0.1',
-  user: 'laogai',
-  password: 'boba',
-  database: 'pokeboba',
+  user: 'pokeboba',
+  password: 'boba123',
+  database: 'PokeBobaDB',
   port: '3306'
 });
 
