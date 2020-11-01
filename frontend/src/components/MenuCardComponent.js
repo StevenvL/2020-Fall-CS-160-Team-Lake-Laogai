0492ce2a45a19fc6c922b0e57074cd586e26e858
Zhijie Xu <jennyxu000@gmail.com>
2020-11-01 07:33:47 -0800

    change Store View page to a functional component

--- frontend/src/components/MenuCardComponent.js
+++ frontend/src/components/MenuCardComponent.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import { Button, Card } from 'react-bootstrap'
+import React from "react";
+import { Button, Card } from "react-bootstrap";
 
-class MenuCardComponent extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div>
-                <Card>
-                    <Card.Img variant="top"  src="/blog1.jpg" />
-                    <Card.Body>
-                        <Card.Title>{this.props.drink.name}</Card.Title>
-                        <Card.Text>
-                            Drink Description
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam ...
-                                </Card.Text>
-                        <Button variant="normal" size="sm">Read More</Button>
-                    </Card.Body>
-                </Card>
-            </div>
-        )
-    }
+function MenuCardComponent({ drink }) {
+  return (
+    <div>
+      <Card>
+        <Card.Img variant="top" src="/blog1.jpg" />
+        <Card.Body>
+          <Card.Title>{drink}</Card.Title>
+          <Card.Text>
+            Drink Description Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam ...
+          </Card.Text>
+          <Button variant="normal" size="sm">
+            Read More
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
-export default MenuCardComponent
+
+export default MenuCardComponent;
