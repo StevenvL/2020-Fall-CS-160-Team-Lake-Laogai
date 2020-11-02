6c8949586c912c98c0df9d638c529e10f1ad2988
yuxiu198 <xiuyutiffany@gmail.com>
2020-11-02 12:34:08 -0800

    adds more logics to helps undersatnd errors and sucess

--- backend/routes/stores.js
+++ backend/routes/stores.js
@@ -1,5 +1,6 @@
 /*Stores Info Backend APIs*/
 
+const { response } = require("express");
 var express = require("express");
 var router = express.Router();
 var connection = require("../sql_conn");
@@ -21,21 +22,46 @@ router.get("/", function (req, res, next) {
   );
 });
 
+
 /* GET stores from store name */
 /* When user select a store name, this function retrive the store infomation of
 the database. It would get the desired store info even if the user only input a partial store name*/
 router.get("/storename/:storeName", function (req, res, next) {
   let storeName = req.params["storeName"];
-  connection.query(
-    `SELECT storeName, street, city, state, zip, avg_rating, menu, sugar_level, ice_level FROM stores 
-    WHERE storeName LIKE '%${storeName}%'`,
-    function (error, results, fields) {
-      if (error) throw error;
-      console.log(results);
-      res.json(results);
-    }
-  );
+  console.log("type of store name: " + typeof(storeName));
+  console.log(storeName);
+  if (storeName !== "") {
+    connection.query(
+      `SELECT storeName, street, city, state, zip, avg_rating, menu, sugar_level, ice_level FROM stores 
+      WHERE storeName LIKE '%${storeName}%'`,
+      function (error, results, fields) {
+        if (error) {
+          console.log("An error occurs.");
+          // bad request
+          res.status(400).json({
+            error: true,
+            message: "No matched stores."
+          });
+        } 
+        else if (res.status(200) && Object.entries(results).length == 0) {
+            res.status(200).json({
+              message: "Sorry, There is no mathced store.",
+              stores: results
+            });
+            console.log ("No matched stores");
+        }
+        else {
+          res.status(200).json({
+            message: "Sucess! You got the stores.",
+            stores: results
+          });
+        }
+        console.log(results);
+      },
+    ); 
+  }  
 });
+
 
 /* GET all drink types */
 /* When user select nothing from the search box or filter, this function retrieves all the stores in the database*/
@@ -54,51 +80,117 @@ router.get("/drinktypes", function (req, res, next) {
   );
 });
 
+
 /* GET stores by drink type */
 /* This function retrieves the corresponding store information based on the drink type filter*/
 router.get("/drinktype/:typename", function (req, res, next) {
   let typename = req.params["typename"];
-  connection.query(
-    `SELECT storeName, street, city, state, zip, avg_rating, menu, sugar_level, 
-    ice_level FROM drink_type JOIN store_drink_type USING(typeID) JOIN stores USING(storeID) WHERE 
-    typename = '${typename}'`,
-    function (error, results, fields) {
-      if (error) throw error;
-      console.log(results);
-      res.json(results);
-    }
-  );
+  if (typename !== "") {
+    connection.query(
+      `SELECT storeName, street, city, state, zip, avg_rating, menu, sugar_level, 
+      ice_level FROM drink_type JOIN store_drink_type USING(typeID) JOIN stores USING(storeID) WHERE 
+      typename = '${typename}'`,
+      function (error, results, fields) {
+        if (error) {
+          console.log("An error occurs.");
+          // bad request
+          res.status(400).json({
+            error: true,
+            message: "No matched stores."
+          });
+        } 
+        else if (res.status(200) && Object.entries(results).length == 0) {
+            res.status(200).json({
+              message: "Sorry, There is no mathced store.",
+              stores: results
+            });
+            console.log ("No matched stores");
+        }
+        else {
+          res.status(200).json({
+            message: "Sucess! You got the stores.",
+            stores: results,
+          });
+        }
+        console.log(results);
+      },
+    ); 
+  }  
 });
+
 
 /* GET stores by city and state */
 /* This function retrieves the desired store information based on the user's city and state input*/
 router.get("/citystate/:city/:state", function (req, res, next) {
   let city = req.params["city"];
   let state = req.params["state"];
-  connection.query(
-    `SELECT storeName, street, city, state, zip, avg_rating, menu, sugar_level, 
-    ice_level FROM stores WHERE city = "${city}" AND state = "${state}"`,
-    function (error, results, fields) {
-      if (error) throw error;
-      console.log(results);
-      res.json(results);
-    }
-  );
+  if (city !== "" && state !== "") {
+    connection.query(
+      `SELECT storeName, street, city, state, zip, avg_rating, menu, sugar_level,
+      ice_level FROM stores WHERE city = "${city}" AND state = "${state}"`,
+      function (error, results, fields) {
+        if (error) {
+          console.log("An error occurs.");
+          // bad request
+          res.status(400).json({
+            error: true,
+            message: "No matched stores."
+          });
+        } 
+        else if (res.status(200) && Object.entries(results).length == 0) {
+            res.status(200).json({
+              message: "Sorry, There is no mathced store.",
+              stores: results
+            });
+            console.log ("No matched stores");
+        }
+        else {
+          res.status(200).json({
+            message: "Sucess! You got the stores.",
+            stores: results
+          });
+        }
+        console.log(results);
+      },
+    ); 
+  }  
 });
+
 
 /* GET stores by zip code*/
 /* This function retrieves the desired store information based on the user's zip input*/
 router.get("/zip/:zip", function (req, res, next) {
   let zip = req.params["zip"];
-  connection.query(
-    `SELECT storeName, street, city, state, zip, avg_rating, menu, sugar_level, 
-    ice_level FROM stores WHERE zip = '${zip}'`,
-    function (error, results, fields) {
-      if (error) throw error;
-      console.log(results);
-      res.json(results);
-    }
-  );
+  if (zip !== "") {
+    connection.query(
+      `SELECT storeName, street, city, state, zip, avg_rating, menu, sugar_level,
+      ice_level FROM stores WHERE zip = ${zip}`,
+      function (error, results, fields) {
+        if (error) {
+          console.log("An error occurs.");
+          // bad request
+          res.status(400).json({
+            error: true,
+            message: "No matched stores."
+          });
+        } 
+        else if (res.status(200) && Object.entries(results).length == 0) {
+            res.status(200).json({
+              message: "Sorry, There is no mathced store.",
+              stores: results
+            });
+            console.log ("No matched stores");
+        }
+        else {
+          res.status(200).json({
+            message: "Sucess! You got the stores.",
+            stores: results
+          });
+        }
+        console.log(results);
+      },
+    ); 
+  }  
 });
 
 /* POST a new store */
