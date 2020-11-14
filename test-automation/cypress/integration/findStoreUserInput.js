94a7bb32176e07c72d9cfc95237d128a144beb27
yuxiu198 <xiuyutiffany@gmail.com>
2020-11-14 11:20:53 -0800

    add test find store input box

--- test-automation/cypress/integration/findStoreUserInput.js
+++ test-automation/cypress/integration/findStoreUserInput.js
@@ -1,20 +1,26 @@
 const findStorePage = 'http://localhost:3000/findStore'
-const testInput = {
-    input0: 'Tiger',
-    input1: 'Tiger Bo',
-    input2: 'Tiger Boba',
-    input3: 'I am a random name'
-}
-describe('Testing Add a New Store Page', () => {
+const testInput = 'Tiger Boba';
+describe('Testing find a store page user input', () => {
     before(() => {
         cy.visit(findStorePage);
-    })
+    });
     
     context('Test if the user input box correctly taking user inputs', () => {    
+        
+        it('should have the search boba store area', () => {   
+            cy.get('.search-boba')
+              .should('have.class', 'search-boba')
+        });
+        
+        it('should have the search boba store input box', () => {   
+            cy.get('.form-control')
+              .should('have.class', 'form-control')
+        });
+        
         it('should accepts store name input', () => {   
-            cy.get('.storeNameInput')
-              .type(testInput.input0)
-              .should('have.value', testInput.input0)
-        })
-    })
-})
+            cy.get('.form-control')
+              .type(testInput)
+              .should('have.value', testInput);
+        });
+    });
+});
