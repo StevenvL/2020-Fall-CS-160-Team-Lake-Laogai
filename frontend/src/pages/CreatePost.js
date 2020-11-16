5d654ffbe2686759ade91ec7facd5d1c5b55a3f9
yuxiu198 <xiuyutiffany@gmail.com>
2020-11-16 14:58:02 -0800

    updated createPost.js

--- frontend/src/pages/CreatePost.js
+++ frontend/src/pages/CreatePost.js
@@ -87,7 +87,7 @@ class CreatePost extends Component {
                         <Form.Control required as="textarea" value = {this.state.postBody} onChange = {e => this.setState({postBody: e.target.value})} />
                     </Form.Group>
 
-                    <Button href='/createpost' variant="normal" type="submit">
+                    <Button variant="normal" type="submit">
                         Submit
                 </Button>
                 </Form>
